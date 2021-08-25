7f7bde6fb0fe17ab429dd71a7162e5fa8de57da3
silikhe <silaswan@gmail.com>
2021-08-25 14:57:12 +0300

    List all users

--- src/main/java/dao/Sql2oUserDao.java
+++ src/main/java/dao/Sql2oUserDao.java
@@ -4,6 +4,9 @@ import models.User;
 import org.sql2o.Connection;
 import org.sql2o.Sql2oException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sql2oUserDao {
     public Sql2oUserDao(){}
 
@@ -31,5 +34,55 @@ public class Sql2oUserDao {
                     .addParameter("id", id)
                     .executeAndFetchFirst(User.class);
         }
+    }
+
+    @Override
+    public List<User> allUsers() {
+        String sql = "SELECT * from users;";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .executeAndFetch(User.class);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from users where id = :id;";
+        try (Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeUpdate();
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        String sql = "DELETE from users;";
+        try (Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .executeUpdate();
+        }
+    }
+
+    @Override
+    public List<News> myNews(int userId){
+        List<News> newsList = new ArrayList<>();
+        String jointSql = "SELECT newsid from departments_news WHERE userid = :userid;";
+        try (Connection con = DB.sql2o.open()) {
+            List<Integer> allIds = con.createQuery(jointSql)
+                    .addParameter("userid",userId)
+                    .executeAndFetch(Integer.class);
+
+            String getSql = "SELECT * FROM news WHERE id = :id;";
+            for(int id:allIds){
+                newsList.add(
+                        con.createQuery(getSql)
+                                .addParameter("id",id)
+                                .executeAndFetchFirst(News.class)
+                );
+            }
+        }
+
+        return newsList;
     }
 }
