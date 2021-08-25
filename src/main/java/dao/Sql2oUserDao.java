097d28e704785068e796351cb6e94070b0986808
silikhe <silaswan@gmail.com>
2021-08-25 14:55:47 +0300

    Add a user to database

--- src/main/java/dao/Sql2oUserDao.java
+++ src/main/java/dao/Sql2oUserDao.java
@@ -1,6 +1,25 @@
 package dao;
 
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
 public class Sql2oUserDao {
     public Sql2oUserDao(){}
+
+
+    @Override
+    public void add(User user) {
+        String sql = "INSERT INTO users (name,position,role,department) VALUES (:name,:position,:role,:department);";
+        try (Connection con = DB.sql2o.open()) {
+            int id = (int) con.createQuery(sql,true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        } catch (Sql2oException ex){
+            System.out.println("User not added: "+ex);
+        }
+    }
 
 }
