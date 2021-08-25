b5d1748a6f0dcc3e52b2e860573a7f5c4f81f09d
silikhe <silaswan@gmail.com>
2021-08-26 01:44:56 +0300

    fetch defaul path

--- src/main/java/dao/SQL2oSitemapDao.java
+++ src/main/java/dao/SQL2oSitemapDao.java
@@ -2,6 +2,8 @@ package dao;
 
 import models.Sitemap;
 import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
 import java.util.List;
 
 public class SQL2oSitemapDao {
@@ -13,6 +15,24 @@ public class SQL2oSitemapDao {
         try (Connection con = DB.sql2o.open()) {
             return con.createQuery(sql)
                     .executeAndFetch(Sitemap.class);
+        }
+    }
+
+//    public static addPaths(int id, cha){
+//
+//    }
+
+    @Override
+    public void addNewsToDepartment(int deptid, int newsid, int userid) {
+        String sql = "INSERT INTO departments_news (deptid,newsid,userid) VALUES (:deptid,:newsid,:userid)";
+        try (Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("deptid",deptid)
+                    .addParameter("newsid",newsid)
+                    .addParameter("userid",userid)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
         }
     }
 }
